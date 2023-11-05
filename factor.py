55c5d68e6c7c0ce4dbe6a1f414e8b6827ce875a6
vaibhavkumawat009 <129666567+vaibhavkumawat009@users.noreply.github.com>
2023-11-05 17:02:50 +0530

    Create factor.py

---
+++ factor.py
@@ -0,0 +1,17 @@
+def factor(number):
+    for i in range(1,number+1):
+        if(number%i==0):
+            print(i)
+        
+    
+    
+n = int(input("give a number: "))
+
+
+(factor(n))
+
+# output give a number: 10
+1
+2
+5
+10
